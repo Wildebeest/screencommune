6208c56322e0f8c059da57be09f06bbf87618fe0
Matt Wilde <matthewcwilde@gmail.com>
2017-11-25 11:33:45 -0800

    Initial Commit

---
+++ ScreenCommune/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenCommune
+//
+//  Created by Matt Wilde on 11/25/17.
+//  Copyright © 2017 Matthew Wilde. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
